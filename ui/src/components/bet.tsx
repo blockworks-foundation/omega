e9b72b0b82e8b7291276a4e2e8d3ebcc47101add
Maximilian Schneider <mail@maximilianschneider.net>
2021-01-25 12:31:31 +0100

    update layout

--- ui/src/components/bet.tsx
+++ ui/src/components/bet.tsx
@@ -13,7 +13,7 @@ import contract_keys from "../contract_keys.json";
 import { markets } from "../markets";
 
 const SBMarketView = (props: {}) => {
-  const colStyle = { padding: "0.5em", width: 256+128+64 };
+  const colStyle = { padding: "0.5em", width: 256+128 };
   const m = markets[0];
 
   return (
@@ -21,7 +21,6 @@ const SBMarketView = (props: {}) => {
       <Row justify="center">
         <Col>
           <div style={colStyle}>
-            <Card>
               <p>
                 <img src={m.outcomes[0].icon} alt={m.outcomes[0].name} width="200px" height="200px"/>
               </p>
@@ -32,12 +31,10 @@ const SBMarketView = (props: {}) => {
                   <BetButton label="" type="primary" market={m} outcome={m.outcomes[0]} />
                 </CurrencyPairProvider>
               </p>
-            </Card>
           </div>
         </Col>
         <Col>
           <div style={colStyle}>
-            <Card>
               <p>
                 <img src={m.outcomes[1].icon} alt={m.outcomes[1].name} width="200px" height="200px"/>
               </p>
@@ -48,7 +45,6 @@ const SBMarketView = (props: {}) => {
                   <BetButton label="" type="primary" market={m} outcome={m.outcomes[1]} />
                 </CurrencyPairProvider>
               </p>
-            </Card>
           </div>
         </Col>
       </Row>
@@ -116,20 +112,23 @@ export const BetView = (props: {}) => {
       <Card
         className="bet-card"
         headStyle={{ padding: 0 }}
-        bodyStyle={{ width: 700 }}
+        bodyStyle={{ width: 900 }}
       >
         <div>
           <h1>Which team will win the 55th Super Bowl?</h1>
+          <Divider />
+          <SBMarketView />
+
           <Row justify="center">
-            <Col>
-              <p>The winner will be resolved by February 8th 08:00 AM UTC according to the results of the championship game. Winning tokens can be redeemed by March 8th 08:00 AM UTC.</p>
+            <Col style={{ width: 256+128+64+64+128+32 }}>
               <Divider />
-              <p><b>Disclaimer:</b> Trading on Omega is not available in the U.S.A, EU or other prohibited jurisdictions. If you are located in, incorporated or otherwise established in, or a resident of the United States of America or any European nation, you are not permitted to trade on Omega.</p>
+              <p>The winner will be resolved by February 8th 08:00 AM UTC according to the results of the championship game. Winning tokens can be redeemed from then until March 8th 08:00 AM UTC.</p>
+              <Divider />
+              <p><b>Disclaimer:</b> Trading on Omega is not available in the U.S.A, E.U. or other prohibited jurisdictions. If you are located in, incorporated or otherwise established in, or a resident of the United States of America or any nation part of the European Union, you are not permitted to trade on Omega.</p>
             </Col>
           </Row>
         </div>
       </Card>
-      <SBMarketView />
     </>
   );
 };
