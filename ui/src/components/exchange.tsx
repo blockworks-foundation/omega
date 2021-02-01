1a554df23dc2cb15f12d5393543a2ab560c236f5
Kehinde Faleye <kenny.fale.kf@gmail.com>
2021-02-02 00:53:47 +0100

    Imported the issueset function in order to create an issue set

--- ui/src/components/exchange.tsx
+++ ui/src/components/exchange.tsx
@@ -5,10 +5,41 @@ import { SettingOutlined } from "@ant-design/icons";
 import { AppBar } from "./appBar";
 import { CurrencyPairProvider } from "../utils/currencyPair";
 import { SwapView } from "./swap";
-import contract_keys from "../contract_keys.json";
+// Issue set helper
+import issueSet from '../utils/issueSet';
 import { markets } from "../markets";
+// Connect to wallet
+import { useWallet } from '../utils/wallet';
+// Create connection to wallet
+import { useConnection } from '../utils/connection';
+// Our contract details
+import contract_keys from "../contract_keys.json";
+import { useMint } from '../utils/accounts';
 
+// TODO: Select market before making exchange
 export const ExchangeView = (props: {}) => {
+  let connection = useConnection();
+  const { wallet } = useWallet();
+  // The mint address public key
+  const quoteMint = useMint(contract_keys.quote_mint_pk);
+
+  const parseAmount = (amount: any) => {
+  if(quoteMint) {
+    try {
+      return parseFloat(amount) * Math.pow(10, quoteMint.decimals);
+    } catch (error) {
+      // TODOl WHat to do here
+    }
+  } else {
+    // TODO: What to do here
+  }
+}
+
+  // Swap usdc for market tokens
+  const callIssueSet = async () => {
+    console.log('Market is ', markets[0])
+    await issueSet(markets[0], parseAmount(1), wallet, connection)
+}
 
   const colStyle: React.CSSProperties = { padding: "1em" };
 
@@ -54,6 +85,11 @@ export const ExchangeView = (props: {}) => {
             </Row>
           </>
         )}
+        <Button  shape="circle"
+              size="large"
+              type="text"
+              onClick={callIssueSet}
+              > Provide Liquidity</Button>
     </>
   );
 };
